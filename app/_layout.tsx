478e361020b0c4260340254d52e7600bd1f16e80
Jonathan Bydendyk <jonathan@hacklab.co.za>
2025-10-27 19:06:33 +0200

    Cleanup imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-import { StatusBar } from 'expo-status-bar';
 import { RobotProvider } from '../contexts/RobotContext';
 
 export default function RootLayout() {
